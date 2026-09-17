b5d838269a052f6c05b88a37a50869f477020c13
dola-cell <zqbx2024@qq.com>
2026-09-17 15:37:38 +0800

    feat: add project entry main.py, auto set python path

---
+++ main.py
@@ -0,0 +1,10 @@
+﻿import sys
+from pathlib import Path
+# 将项目根目录加入Python搜索路径
+sys.path.append(str(Path(__file__).parent))
+
+from src.agent.customer_agent import EcommerceRAGAgent
+
+if __name__ == "__main__":
+    agent = EcommerceRAGAgent()
+    agent.chat_loop()
