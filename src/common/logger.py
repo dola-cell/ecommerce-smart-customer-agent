7bac645bac9617578ecead8017adb8d73024a5eb
dola-cell <zqbx2024@qq.com>
2026-09-17 15:33:12 +0800

    feat: add global log module

---
+++ src/common/logger.py
@@ -0,0 +1,10 @@
+﻿from loguru import logger
+import sys
+
+# 企业级日志配置
+logger.remove()
+logger.add(sys.stdout, format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", level="INFO")
+logger.add("logs/runtime.log", rotation="500 MB", retention="10 days", level="INFO")
+
+if __name__ == "__main__":
+    logger.info("Logger init success")
